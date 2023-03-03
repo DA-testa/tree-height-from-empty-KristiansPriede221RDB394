7b4288ace051fb2a1017aef778d94ba00111ac0e
KristiansPriede221RDB394 <Kristians.Priede@edu.rtu.lv>
2023-03-03 15:25:39 +0200

    trf3

--- tree_height.py
+++ tree_height.py
@@ -1,26 +1,36 @@
 # python3
 import sys
 import threading
+path="/home/runner/work/tree-height-from-empty-KristiansPriede221RDB394/tree-height-from-empty-KristiansPriede221RDB394/"
+##path="C:\\Users\\Kristians\\Desktop\\pitons_vs\\tree-height-from-empty-KristiansPriede221RDB394\\test\\"
 check=input()
-if check.startswith('I')or check.startswith('F'):
+if check.startswith('I'):
      n=int(input())
-     parents=list(map(int,input().split(' ')))
+     parents=list(map(int,input().split(' '))) 
+elif check.startswith('F'):
+     filename=input()
+     if filename.__contains__("a")==False:
+               ptf=path+filename
+               with open(ptf,'r')as file:
+                fl=file.read()
+                n=int(fl.split('\n')[0])
+                parents_str=fl.split('\n')[1]
+                parents=list(map(int,parents_str.split(' ')))
+                                
 class Tree():
-    def get(self,n,parents):
-            self.n=n
-            self.parents=parents
+    def get(k,n,parents):
+            k.n=n
+            k.parents=parents
             pass
-
-    def compute_height(self,n,parents):
+    def compute_height(k,n,parents):
         max_heigth=0
-        for i in range(self.n):
+        for i in range(k.n):
             val=0
             j=i
             while j!=-1:
                 val=val+1
-                j=self.parents[j]
+                j=k.parents[j]
             max_heigth=max(max_heigth,val)   
-
         return max_heigth 
     pass
 def main():
